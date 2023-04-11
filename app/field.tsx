6a2232110bd40126937c394fdc694200d00cb15c
Aleksey Vokhmin <vohmina2011@yandex.ru>
2023-04-11 13:53:59 +0300

    ?

--- app/field.tsx
+++ app/field.tsx
@@ -101,7 +101,9 @@ export default function Field({
 
   useEffect(() => {
     if (!centralObjectRef.current) return;
-    centralObjectRef.current.style.backgroundImage = `url("/assets/images/${centralObject}.png")`;
+    centralObjectRef.current.style.backgroundImage = `url("/assets/images/${encodeURIComponent(
+      centralObject
+    )}.png")`;
   }, [centralObject]);
 
   useEffect(() => {
